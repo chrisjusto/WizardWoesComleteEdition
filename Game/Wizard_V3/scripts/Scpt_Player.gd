73e0303490e5c2c8e4793c47603fbc09be9921a2
chrisjusto <chris.justo96@gmail.com>
2023-01-05 20:40:41 -0800

    Update

--- Game/Wizard_V3/scripts/Scpt_Player.gd
+++ Game/Wizard_V3/scripts/Scpt_Player.gd
@@ -13,6 +13,20 @@ func _jump_input():
 	motion.y = -jump_velocity[0]
 	jump_interval += 1
 	airiel_action = airiel_states.Jumping
+	
+func _jump_action():
+	if jump_interval <= jump_velocity.size()-1:
+		motion.y= jump_velocity[jump_interval]
+		jump_interval += 1
+	else:
+		pass
+
+func _jump_end():
+	motion.y = jump_end_velocity
+	jump_interval = 0
+	airiel_action = airiel_states.Falling
+	pass
+	
 
 func _ready():
 	game_manager._register_player(self)
@@ -25,7 +39,12 @@ func _physics_process(delta):
 	#call the jump input
 	if Input.is_action_just_pressed("jump") and base_action == base_states.On_Ground:
 		_jump_input()
+		base_action = base_states.In_Air
+		airiel_action = airiel_states.Jumping
 		pass
+	
+	if airiel_action == airiel_states.Jumping:
+		_jump_action()
 		
 	#call movement right and left or return to standing
 	if Input.is_action_pressed("right"):
